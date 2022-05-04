0f4691eda8cc3683fb170644524d645b467395eb
Lily319 <leftbehindgd2009@gmail.com>
2022-05-04 19:00:40 +0800

    0504_1

--- LinqLabs/5. FrmLinq_To_Entity.cs
+++ LinqLabs/5. FrmLinq_To_Entity.cs
@@ -16,15 +16,114 @@ namespace Starter
         public FrmLinq_To_Entity()
         {
             InitializeComponent();
+
+            Console.Write("xxx...open()...select*from...close()...");
+            dbContext.Database.Log = Console.Write;
         }
 
+        NorthwindEntities dbContext = new NorthwindEntities();
         private void button1_Click(object sender, EventArgs e)
         {
+            dataGridView1.DataSource = null;
+            //加入新項目>>
+            //(修改)MODEL按右鍵>>從資料庫更新模型
             NorthwindEntities dbContext = new NorthwindEntities();
             var q = from p in dbContext.Products
                     where p.UnitPrice > 30
                     select p;
             dataGridView1.DataSource = q.ToList();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = dbContext.Categories.First().Products.ToList();
+            MessageBox.Show(dbContext.Products.First().Category.CategoryName);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            //MODEL按右鍵>>從資料庫更新模型>>加入預存程序>>會變成方法
+            dataGridView1.DataSource = dbContext.Sales_by_Year(new DateTime(1997, 1, 1), DateTime.Now).ToList();
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            var q = from p in dbContext.Products.AsEnumerable()
+                    orderby p.UnitsInStock descending, p.ProductID
+                    select new
+                    {
+                        p.ProductName,
+                        p.UnitPrice,
+                        p.UnitsInStock,
+                        TotalPrice=$"{p.UnitPrice*p.UnitsInStock:c2}" //未加.AsEnumerable()時 無法格式化
+                    };
+            dataGridView1.DataSource = q.ToList();
+
+            //等於以下
+
+            var q1 = dbContext.Products.OrderByDescending(p => p.UnitsInStock).ThenBy(p => p.ProductID);
+            dataGridView2.DataSource = q1.ToList();
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            dataGridView3.DataSource = null;
+            var q = dbContext.Products.Select(p => new
+            {
+                p.CategoryID,
+                p.Category.CategoryName,
+                p.ProductName,
+                p.UnitPrice
+            });
+            dataGridView3.DataSource = q.ToList();
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            //inner join
+            var q = from c in dbContext.Categories
+                    from p in c.Products
+                    select new
+                    {
+                        c.CategoryID,
+                        c.CategoryName,
+                        p.ProductName,
+                        p.UnitPrice
+                    };
+            dataGridView1.DataSource = q.ToList();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            var q = from p in dbContext.Products.AsEnumerable()
+                    group p by p.Category.CategoryName into g
+                    select new
+                    {
+                        CategoryName = g.Key,
+                        Avg = $"{g.Average(p => p.UnitPrice):c2}"
+                    };
+            dataGridView1.DataSource = q.ToList();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            var q = dbContext.Orders.GroupBy(o => o.OrderDate.Value.Year).Select(g => new
+            {
+                Year = g.Key,
+                Count = g.Count()
+            });
+            dataGridView1.DataSource = q.ToList();
+        }
+
+        private void button55_Click(object sender, EventArgs e)
+        {
+            Product prod = new Product { ProductName = DateTime.Now.ToLongTimeString(), Discontinued = true };
+            dbContext.Products.Add(prod);//在記憶體
+            dbContext.SaveChanges();//儲存變更到實體資料庫
         }
     }
 }
--- LinqLabs/作業/Frm作業_3.cs
+++ LinqLabs/作業/Frm作業_3.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LinqLabs;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Forms;
 
 namespace MyHomeWork
@@ -18,10 +20,16 @@ namespace MyHomeWork
             InitializeComponent();
         }
 
+        private void AllClean()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView2.DataSource = null;
+            treeView1.Nodes.Clear();
+        }
         private void button4_Click(object sender, EventArgs e)
         {
-            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-            treeView1.Nodes.Clear();
+            AllClean();
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 15, 14, 13, 12, 11, 10 };
             TreeNode node = null;
             int count = 0;
             foreach(int n in nums)
@@ -66,7 +74,6 @@ namespace MyHomeWork
             //    }
             //}
         }
-
         private string MyNumsKey(int n)
         {
             if (n < 5) return "Small";
@@ -74,25 +81,24 @@ namespace MyHomeWork
             else return "Large";
         }
 
+        IEnumerable<FileInfo> q1;
+        int flag;
         private void button38_Click(object sender, EventArgs e)
         {
-            dataGridView1.DataSource = null;
-            dataGridView2.DataSource = null;
-            treeView1.Nodes.Clear();
+            AllClean();
+            flag = 1;
             DirectoryInfo dir = new DirectoryInfo(@"C:\windows");
             FileInfo[] files = dir.GetFiles();
-            dataGridView1.DataSource = files.OrderByDescending(f => f.Length).ToList();
-            var q = from f in files
-                    orderby f.Length descending
-                    group f by MyLengthKey(f) into g
-                    select new
-                    {
-                        Length= g.Key,
-                        Count= g.Count(),
-                        Group=g
-                    };
-            dataGridView2.DataSource = q.ToList();
+            var q = files.OrderByDescending(f => f.Length).GroupBy(f => MyLengthKey(f)).Select(g => new
+            {
+                Length = g.Key,
+                Count = g.Count(),
+                Group = g
+            });
+            dataGridView1.DataSource = q.ToList();
 
+            q1 = files.Where(f => MyLengthKey(f) == dataGridView1.CurrentRow.Cells[0].Value).OrderByDescending(f=>f.Length);
+            dataGridView2.DataSource = q1.ToList();
             foreach (var group in q)
             {
                 string s = $"{group.Length}({group.Count})";
@@ -103,7 +109,6 @@ namespace MyHomeWork
                 }
             }
         }
-
         private object MyLengthKey(FileInfo f)
         {
             if (f.Length < 1000) return "Small";
@@ -113,12 +118,10 @@ namespace MyHomeWork
 
         private void button6_Click(object sender, EventArgs e)
         {
-            dataGridView1.DataSource = null;
-            dataGridView2.DataSource = null;
-            treeView1.Nodes.Clear();
+            AllClean();
+            flag = 1;
             DirectoryInfo dir = new DirectoryInfo(@"C:\windows");
             FileInfo[] files = dir.GetFiles();
-            dataGridView1.DataSource = files.OrderByDescending(f => f.CreationTime.Year).ToList();
             var q = from f in files
                     orderby f.CreationTime.Year descending
                     group f by f.CreationTime.Year into g
@@ -128,8 +131,9 @@ namespace MyHomeWork
                         Count = g.Count(),
                         Group = g
                     };
-            dataGridView2.DataSource = q.ToList();
-
+            dataGridView1.DataSource = q.ToList();
+            q1 = files.Where(f => f.CreationTime.Year == (int)(dataGridView1.CurrentRow.Cells[0].Value)).OrderByDescending(f=>f.CreationTime.Year);
+            dataGridView2.DataSource = q1.ToList();
             foreach (var group in q)
             {
                 string s = $"{group.Year}({group.Count})";
@@ -139,6 +143,147 @@ namespace MyHomeWork
                     node.Nodes.Add(items.ToString());
                 }
             }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (flag == 1)
+            {
+                dataGridView2.DataSource = q1.ToList();
+            }
+            if (flag == 2)
+            {
+                dataGridView2.DataSource = q2.ToList();
+            }
+            if (flag == 3)
+            {
+                dataGridView2.DataSource = q3.ToList();
+            }
+        }
+
+        NorthwindEntities dbContext = new NorthwindEntities();
+        IEnumerable<Product> q2;
+        private void button8_Click(object sender, EventArgs e)
+        {
+            AllClean();
+            flag = 2;
+            var q = dbContext.Products.AsEnumerable().GroupBy(p => MyUnitPriceKey(p)).Select(g => new
+            {
+                Key= g.Key,
+                Count= g.Count(),
+                Group=g
+            });
+            dataGridView1.DataSource = q.ToList();
+            q2 = dbContext.Products.AsEnumerable().Where(p => MyUnitPriceKey(p) == dataGridView1.CurrentRow.Cells[0].Value);
+            dataGridView2.DataSource = q2.ToList();
+
+            foreach (var group in q)
+            {
+                string s = $"{group.Key}({group.Count})";
+                TreeNode node = treeView1.Nodes.Add(group.Key.ToString(), s);
+                foreach (var items in group.Group)
+                {
+                    node.Nodes.Add(items.ProductName);
+                }
+            }
+        }
+        private object MyUnitPriceKey(Product p)
+        {
+            if (p.UnitPrice < 20) return "Low";
+            else if (p.UnitPrice > 50) return "High";
+            else return "Medium";
+        }
+
+        IEnumerable<Order> q3;
+        private void button15_Click(object sender, EventArgs e)
+        {
+            AllClean();
+            flag = 3;
+            var q = dbContext.Orders.AsEnumerable().GroupBy(o =>o.OrderDate.Value.Year).Select(g => new
+            {
+                Year = g.Key,
+                Count = g.Count(),
+                Group = g
+            });
+            dataGridView1.DataSource = q.ToList();
+            q3 = dbContext.Orders.AsEnumerable().Where(o => o.OrderDate.Value.Year == (int)(dataGridView1.CurrentRow.Cells[0].Value));
+            dataGridView2.DataSource = q3.ToList();
+
+            foreach (var group in q)
+            {
+                string s = $"{group.Year}({group.Count})";
+                TreeNode node = treeView1.Nodes.Add(group.Year.ToString(), s);
+                foreach (var items in group.Group)
+                {
+                    node.Nodes.Add(items.OrderID.ToString());
+                }
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            AllClean();
+            flag = 3;
+            var q = dbContext.Orders.AsEnumerable().GroupBy(o => o.OrderDate.Value.ToString("yyyy年MM月")).Select(g => new
+            {
+                Month = g.Key,
+                Count = g.Count(),
+                Group = g
+            });
+            dataGridView1.DataSource = q.ToList();
+            q3 = dbContext.Orders.AsEnumerable().Where(o => o.OrderDate.Value.ToString("yyyy年MM月") == dataGridView1.CurrentRow.Cells[0].Value.ToString());
+            dataGridView2.DataSource = q3.ToList();
+
+            foreach (var group in q)
+            {
+                string s = $"{group.Month}({group.Count})";
+                TreeNode node = treeView1.Nodes.Add(group.Month.ToString(), s);
+                foreach (var items in group.Group)
+                {
+                    node.Nodes.Add(items.OrderID.ToString());
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AllClean();
+            float SumTotalPrice = dbContext.Order_Details.AsEnumerable().Sum(od => (float)od.UnitPrice * od.Quantity * (1 - od.Discount));
+            MessageBox.Show($"總銷售金額{SumTotalPrice}");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AllClean();
+            var q = (dbContext.Order_Details.AsEnumerable().GroupBy(od => $"{od.Order.Employee.FirstName} {od.Order.Employee.LastName}")
+                .Select(g => new
+                {
+                    employee = g.Key,
+                    performance = $"{g.Sum(od => (float)od.UnitPrice * od.Quantity * (1 - od.Discount)):C2}"
+                }).OrderByDescending(g => decimal.Parse(g.performance,System.Globalization.NumberStyles.Currency))).Take(5);
+            dataGridView1.DataSource = q.ToList();
+            dataGridView1.AutoResizeColumns();
+        }
+
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            AllClean();
+            var q = dbContext.Products.OrderByDescending(p => p.UnitPrice).Select(p=>new 
+            {
+                p.ProductName,
+                p.Category.CategoryName,
+                p.UnitPrice
+            }).Take(5);
+            dataGridView1.DataSource = q.ToList();
+            dataGridView1.AutoResizeColumns();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            AllClean();
+            bool result = dbContext.Products.Any(p => p.UnitPrice > 300);
+            MessageBox.Show(result ? "有" : "沒有");
         }
     }
 }
